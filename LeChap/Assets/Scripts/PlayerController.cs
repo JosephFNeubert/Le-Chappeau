c7a6381db1342b43fc157f55b8443a6d84080bf5
Joseph Neubert <joeyfneubert@gmail.com>
2024-12-11 01:11:36 -0700

    Added spawning into the game for the players in lobby

---
+++ LeChap/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PlayerController : MonoBehaviourPunCallbacks
+{
+    [HideInInspector]
+    public int id;
+
+    [Header("Info")]
+    public float moveSpeed;
+    public float jumpForce;
+    public GameObject hatObject;
+
+    [HideInInspector]
+    public float currentHatTime;
+
+    [Header("Components")]
+    public Rigidbody _rb;
+    public Player photonPlayer;
+
+    [PunRPC]
+    public void Initialize(Player player)
+    {
+        photonPlayer = player;
+        id = player.ActorNumber;
+
+        GameManager.instance.players[id - 1] = this;
+
+        if (id == 1)
+        {
+            GameManager.instance.GiveHat(id, true);
+        }
+
+        if (!photonView.IsMine)
+        {
+            _rb.isKinematic = true;
+        }
+    }
+
+    void Update()
+    {
+        Move();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TryJump();
+        }
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal") * moveSpeed;
+        float z = Input.GetAxis("Vertical") * moveSpeed;
+
+        _rb.velocity = new Vector3(x, _rb.velocity.y, z);
+    }
+
+    void TryJump()
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+
+        if (Physics.Raycast(ray, 0.7f))
+        {
+            _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    public void SetHat(bool hasHat)
+    {
+        hatObject.SetActive(hasHat);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!photonView.IsMine)
+        {
+            return;
+        }
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (GameManager.instance.GetPlayer(collision.gameObject).id == GameManager.instance.playerWithHat)
+            {
+                if (GameManager.instance.CanGetHat())
+                {
+                    GameManager.instance.photonView.RPC("GiveHat", RpcTarget.All, id, false);
+                }
+            }
+        }
+    }
+}
